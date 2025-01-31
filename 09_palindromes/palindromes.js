8856c5e28a8c9f5f998509a081c8d8a390ea853c
antopix <antopix33@gmail.com>
2025-01-30 16:28:43 -0800

    all exercises complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+  let filteredArr = str.toLowerCase().split("").filter(char => /[a-z0-9]/.test(char));
+  let filtered = filteredArr.join("");
+  let result = filteredArr.reverse().join("") === filtered;
+
+  return result;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
